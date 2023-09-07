919c38f0c88225f2a9bbb99954085f03891fa3c6
k주명 <kevin0019@naver.com>
2023-09-07 18:06:13 +0900

    지웁니다
    지워요

--- HENOAIROCK/src/main/java/com/heno/airock/service/ChatResService.java
+++
@@ -1,8 +0,0 @@
-package com.heno.airock.service;
-
-import com.heno.airock.cmn.WorkDiv;
-import com.heno.airock.dto.ChatRespondVO;
-
-public interface ChatResService extends WorkDiv<ChatRespondVO>{
-
-}
--- HENOAIROCK/src/main/java/com/heno/airock/service/CodeService.java
+++
@@ -1,8 +0,0 @@
-package com.heno.airock.service;
-
-import com.heno.airock.cmn.WorkDiv;
-import com.heno.airock.dto.CodeVO;
-
-public interface CodeService extends WorkDiv<CodeVO>{
-
-}
